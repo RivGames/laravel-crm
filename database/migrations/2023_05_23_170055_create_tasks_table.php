946222f51fe1cbf11d9d8bdb4481f9dbafbf4fb5
Ihor Rud <Igorryd7@gmail.com>
2023-05-23 20:38:29 +0300

    Виконана міграція

---
+++ database/migrations/2023_05_23_170055_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('project_id')->constrained();
+            $table->date('deadline');
+            $table->enum('status',[Status::OPEN->name,Status::IN_PROGRESS->name,Status::CLOSED->name]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
