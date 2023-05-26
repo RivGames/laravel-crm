9b8dcee730740d1a234ad7ce77f6ac95c5e8054a
Ihor Rud <Igorryd7@gmail.com>
2023-05-26 19:07:55 +0300

    Реалізовано CRUD для user

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -8,66 +8,73 @@ use App\Http\Requests\User\UpdateRequest;
 use App\Http\Resources\User\UserCollection;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @param UserService $userService
+     * @return UserCollection
      */
-    public function index()
+    public function index(UserService $userService): UserCollection
     {
-        return new UserCollection(User::all());
+        return $userService->getUserCollection();
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param StoreRequest $request
+     * @param UserService $userService
+     * @return JsonResponse
      */
-    public function store(StoreRequest $request)
+    public function store(StoreRequest $request, UserService $userService): JsonResponse
     {
-        $userData = $request->validated();
-
-        User::create([
-            'name' => $userData['name'],
-            'email' => $userData['email'],
-            'password' => $userData['password'],
-            'role_id' => $userData['role_id'],
-        ]);
+        $userService->createUser($request->validated());
 
         return response()->json([
-            'message' => 'User successfully created!'
+            'message' => 'User successfully created!',
         ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
+     * @param User $user
+     * @param UserService $userService
+     * @return UserResource
      */
-    public function show(User $user)
+    public function show(User $user, UserService $userService): UserResource
     {
-        return new UserResource($user);
+        return $userService->getUserResource($user);
     }
 
     /**
      * Update the specified resource in storage.
+     * @param UpdateRequest $request
+     * @param User $user
+     * @param UserService $userService
+     * @return JsonResponse
      */
-    public function update(UpdateRequest $request, User $user)
+    public function update(UpdateRequest $request, User $user, UserService $userService): JsonResponse
     {
-        $userData = $request->validated();
-
-        $user->update($userData);
+        $userService->updateUser($user, $request->validated());
 
         return response()->json([
-            'message' => 'User successfully updated!'
+            'message' => 'User successfully updated!',
         ]);
     }
 
     /**
      * Remove the specified resource from storage.
+     * @param User $user
+     * @param UserService $userService
+     * @return JsonResponse
      */
-    public function destroy(User $user)
+    public function destroy(User $user, UserService $userService): JsonResponse
     {
-        $user->delete();
+        $userService->deleteUser($user);
 
         return response()->json([], 204);
     }
