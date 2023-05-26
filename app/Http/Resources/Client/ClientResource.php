e2fcedf7b38099b05ced63aa9028c837592ad4d7
Ihor Rud <Igorryd7@gmail.com>
2023-05-26 21:27:14 +0300

    Реалізовано CRUD для clients

---
+++ app/Http/Resources/Client/ClientResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Client;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'company_name' => $this->company_name,
+            'company_vat' => $this->company_vat,
+            'address' => $this->address
+        ];
+    }
+}
