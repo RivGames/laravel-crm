d26fbaad71567038777a40c9d2376b089a365e5e
Ihor Rud <Igorryd7@gmail.com>
2023-05-26 19:07:40 +0300

    Видалив непотрібний комент

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,4 @@
 use App\Http\Controllers\Web\ForbiddenController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::any('{all}', ForbiddenController::class)->where('all','^(?!api).*$');
+Route::any('{all}', ForbiddenController::class)->where('all', '^(?!api).*$');
