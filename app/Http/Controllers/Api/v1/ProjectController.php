4ffcb4d84b8071945f052334ac8a03545164eddb
Ihor Rud <Igorryd7@gmail.com>
2023-05-28 22:05:40 +0300

    Реалізував CRUD для projects

--- app/Http/Controllers/Api/v1/ProjectController.php
+++ app/Http/Controllers/Api/v1/ProjectController.php
@@ -3,47 +3,79 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Project\StoreRequest;
+use App\Http\Requests\Project\UpdateRequest;
+use App\Http\Resources\Project\ProjectCollection;
+use App\Http\Resources\Project\ProjectResource;
+use App\Models\Project;
+use App\Services\ProjectService;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @param ProjectService $projectService
+     * @return ProjectCollection
      */
-    public function index()
+    public function index(ProjectService $projectService): ProjectCollection
     {
-        //
+        return $projectService->getProjectCollection();
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param StoreRequest $request
+     * @param ProjectService $projectService
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request, ProjectService $projectService): JsonResponse
     {
-        //
+        $projectService->createProject($request->validated());
+
+        return response()->json([
+            'message' => 'Project successfully created!'
+        ],Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
+     * @param Project $project
+     * @param ProjectService $projectService
+     * @return ProjectResource
      */
-    public function show(string $id)
+    public function show(Project $project, ProjectService $projectService): ProjectResource
     {
-        //
+        return $projectService->getProjectResource($project);
     }
 
     /**
      * Update the specified resource in storage.
+     * @param UpdateRequest $request
+     * @param Project $project
+     * @param ProjectService $projectService
+     * @return JsonResponse
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request, Project $project, ProjectService $projectService): JsonResponse
     {
-        //
+        $projectService->updateProject($request->validated(),$project);
+
+        return response()->json([
+            'message' => 'Project successfully updated!',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
+     * @param Project $project
+     * @param ProjectService $projectService
+     * @return JsonResponse
      */
-    public function destroy(string $id)
+    public function destroy(Project $project, ProjectService $projectService): JsonResponse
     {
-        //
+        $projectService->deleteProject($project);
+
+        return response()->json([],204);
     }
 }
---
+++ app/Services/ProjectService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\Project\ProjectCollection;
+use App\Http\Resources\Project\ProjectResource;
+use App\Models\Project;
+
+class ProjectService
+{
+    /**
+     * @return ProjectCollection
+     */
+    public function getProjectCollection(): ProjectCollection
+    {
+        return new ProjectCollection(Project::all());
+    }
+
+    public function createProject(array $userData)
+    {
+        Project::create([
+            'title' => $userData['title'],
+            'description' => $userData['description'],
+            'user_id' => $userData['user_id'],
+            'client_id' => $userData['client_id'],
+            'deadline' => $userData['deadline'],
+            'status' => $userData['status'],
+        ]);
+    }
+
+    public function getProjectResource(Project $project)
+    {
+        return new ProjectResource($project);
+    }
+
+    public function updateProject(array $userData, Project $project)
+    {
+        $project->update($userData);
+    }
+
+    public function deleteProject(Project $project)
+    {
+        $project->delete();
+    }
+}
