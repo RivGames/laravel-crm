4a6cc65cfe6a111f240b764167dc977e73de2d90
Ihor Rud <Igorryd7@gmail.com>
2023-05-28 22:07:00 +0300

    Ресурс та колекція для projects

---
+++ app/Http/Resources/Project/ProjectResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Project;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'user_id' => $this->user_id,
+            'client_id' => $this->client_id,
+            'deadline' => $this->deadline,
+            'status' => $this->status
+        ];
+    }
+}
