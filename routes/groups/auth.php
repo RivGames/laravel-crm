e8e7346104448872df32c0973175f632c3293ca6
Ihor Rud <Igorryd7@gmail.com>
2023-06-02 19:33:45 +0300

    Добавлено роути для регістрації, логіну та логауту

---
+++ routes/groups/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register','register')->name('auth.register');
+    Route::post('login','login')->name('auth.login');
+});
+Route::post('logout',[AuthController::class,'logout'])->name('auth.logout')->middleware(['auth:sanctum']);
