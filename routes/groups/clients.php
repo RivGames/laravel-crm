9fa2eb51f1bc7cdb95a669d988547f900b5fd14b
Ihor Rud <Igorryd7@gmail.com>
2023-06-02 19:34:17 +0300

    Обернув роути в middleware auth:sanctum

--- routes/groups/clients.php
+++ routes/groups/clients.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Api\v1\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('clients', ClientController::class);
+    Route::apiResource('clients', ClientController::class)->middleware(['auth:sanctum']);
 });
