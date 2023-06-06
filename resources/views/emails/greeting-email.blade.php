00eb9b01c6f9e83c07bae9769981bcea97604bbd
Ihor Rud <Igorryd7@gmail.com>
2023-06-06 21:31:36 +0300

    Додано тайтл до емейлу

--- resources/views/emails/greeting-email.blade.php
+++ resources/views/emails/greeting-email.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email</title>
 </head>
 <body>
 <div style="background-color: black; padding: 20px;">
