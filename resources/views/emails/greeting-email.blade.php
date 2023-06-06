a08a6178ba946ee3525f51a141ae30fa9a5adadd
Ihor Rud <Igorryd7@gmail.com>
2023-06-06 21:30:07 +0300

    Базовий емейл

---
+++ resources/views/emails/greeting-email.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div style="background-color: black; padding: 20px;">
+    <h1 style="text-align: center; color: white;">Welcome to Laravel CRM!</h1>
+    <p style="font-size: 16px; color: white; text-align: center;">Thank you for joining us.</p>
+
+    <hr style="margin: 20px 0;">
+
+    <p style="font-size: 14px; color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum varius dolor eu elit rutrum, eget pellentesque ipsum iaculis. Nulla facilisi. Phasellus dapibus lectus vitae ante bibendum, sit amet eleifend urna tincidunt.</p>
+
+    <p style="font-size: 14px; color: white;">In hac habitasse platea dictumst. Sed rutrum pharetra vulputate. Nam non orci vel lectus porttitor feugiat at sed est. Fusce faucibus massa in justo tincidunt, ac congue elit laoreet. Nulla bibendum ipsum at nunc pellentesque, vitae lacinia nulla egestas. Integer elementum faucibus ante vitae condimentum.</p>
+
+    <p style="font-size: 14px; color: white;">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In ac fermentum ex. Morbi bibendum, sapien sed vulputate mollis, felis urna vulputate purus, vel iaculis elit lectus sit amet lacus. Proin id scelerisque risus.</p>
+
+    <p style="font-size: 14px; color: white;">Thank you again for joining us!</p>
+
+    <p style="font-size: 14px; color: white;">Best regards,<br>Our Team</p>
+</div>
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,7 @@ require __DIR__.'/groups/projects.php';
 require __DIR__.'/groups/tasks.php';
 
 require __DIR__.'/groups/auth.php';
+
+\Illuminate\Support\Facades\Route::get('mail',function(){
+    \Illuminate\Support\Facades\Mail::to('Igorryd6@gmail.com')->send(new \App\Mail\GreetingEmail());
+});
