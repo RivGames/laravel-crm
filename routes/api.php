05dd28f6bf6fcd7a0a97fc0b2cad91e38373a484
Ihor Rud <Igorryd7@gmail.com>
2023-06-07 18:57:51 +0300

    Прибрано непотрібний роут

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,17 @@
 <?php
 
-require __DIR__.'/groups/app.php';
+require __DIR__ . '/groups/app.php';
 
-require __DIR__.'/groups/users.php';
+require __DIR__ . '/groups/users.php';
 
-require __DIR__.'/groups/clients.php';
+require __DIR__ . '/groups/clients.php';
 
-require __DIR__.'/groups/projects.php';
+require __DIR__ . '/groups/projects.php';
 
-require __DIR__.'/groups/tasks.php';
+require __DIR__ . '/groups/tasks.php';
 
-require __DIR__.'/groups/auth.php';
+require __DIR__ . '/groups/auth.php';
 
-\Illuminate\Support\Facades\Route::get('mail',function(){
+\Illuminate\Support\Facades\Route::get('mail', function () {
     \Illuminate\Support\Facades\Mail::to('Igorryd6@gmail.com')->send(new \App\Mail\GreetingEmail());
 });
